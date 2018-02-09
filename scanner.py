a34a7b0d5cc688855f2fba944bf1b4edca001b94
darek <darek@cosmose.co>
2018-02-09 13:25:19 +0100

    Added time and settings

--- scanner.py
+++ scanner.py
@@ -1,5 +1,6 @@
 import os
 import re
+import time
 
 class Scanner():
 
@@ -8,9 +9,20 @@ class Scanner():
 	def runScan(self):
 		count = 0
 		while (count < 2):
+			start = self.getMillis()
+			
 			output = os.popen(self.command).read()
 			print self.parseOutput(output)
+
+			self.printTimeDiff(start)
 			count = count + 1
 
 	def parseOutput(self, output):
 		return "\n" + re.sub('(?m)^\s+', '', output)
+
+	def getMillis(self):
+		return int(round(time.time() * 1000))
+
+	def printTimeDiff(self, start):
+		difference = self.getMillis() - start
+		print "%dms" % difference 

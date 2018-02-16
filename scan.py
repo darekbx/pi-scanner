16a159c33144fe007b69b5a03b8a2d22966b4718
darek <darek@cosmose.co>
2018-02-16 11:32:02 +0100

    bt attampt

--- scan.py
+++ scan.py
@@ -1,4 +1,19 @@
 from scanner import Scanner
+from bluepy.bluepy.btle import UUID, Peripheral
 
-scanner = Scanner()
-scanner.runScan()
+temp_uuid = UUID(0x2221)
+p = Peripheral("A8:96:75:32:9D:EB", "random")
+
+try:
+	ch = p.getCharacteristics(uuid=temp_uuid)[0]
+    if (ch.supportsRead()):
+        while 1:
+			value = ch.read()
+            print str(val)
+
+			#scanner = Scanner()
+			#scanner.runScan()
+            time.sleep(1)
+
+finally:
+    p.disconnect()

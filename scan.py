4d0c7b082c1153c61900a8d95bae736c56cb5bad
darek <darek@cosmose.co>
2018-02-16 13:04:07 +0100

    gatt fix

--- scan.py
+++ scan.py
@@ -1,22 +1,18 @@
 from scanner import Scanner
 import pygatt
 
-temp_uuid = UUID(0x2221)
-p = Peripheral("A8:96:75:32:9D:EB", "random")
-
 adapter = pygatt.BGAPIBackend()
 
 try:
-    adapter.start()
-    device = adapter.connect('A8:96:75:32:9D:EB')
-
+	adapter.start()
+	device = adapter.connect('A8:96:75:32:9D:EB')
 	while 1:
-    	value = device.char_read("00001105-0000-1000-8000-00805f9b34fb")
+		value = device.char_read("00001105-0000-1000-8000-00805f9b34fb")
 		print str(val)
 		time.sleep(1)
 
 finally:
-    p.disconnect()
+    adapter.stop()
 
 #scanner = Scanner()
 #scanner.runScan()

ace0b9128cbaea6c7852d33529255f8abf55bb2d
darek <darek@cosmose.co>
2018-10-08 14:42:51 +0200

    Base64 encoding

--- scan.py
+++ scan.py
@@ -1,21 +1,8 @@
 from scanner import Scanner
-""" import pygatt
+import zlib
+import base64
 
-adapter = pygatt.GATTToolBackend()
+scanner = Scanner()
+resultJson = scanner.runScan()
 
-try:
-	adapter.start()
-	device = adapter.connect('A8:96:75:32:9D:EB')
-	print "after device"
-	while 1:
-		print "waiting for read..."
-		value = device.char_read("00002a00-0000-1000-8000-00805f9b34fb")
-		print str(val)
-		time.sleep(1)
-
-finally:
-    adapter.stop() """
-
-print "123456789012345678abcdefghi"
-#scanner = Scanner()
-#scanner.runScan()
+print base64.b64encode(resultJson)
--- scanner.py
+++ scanner.py
@@ -13,7 +13,7 @@ class Scanner():
 	commandCosmose = "sudo iw dev wlp4s0 scan | egrep \"" + searchArguments + "\""
 	commandPi = "sudo iw dev wlan0 scan | egrep \"" + searchArguments + "\""
 	
-	isForPI = True
+	isForPI = False
 
 	def runScan(self):
 		fingerprintCount = 0
@@ -36,29 +36,10 @@ class Scanner():
 			
 			fingerprintCount = fingerprintCount + 1
 			
-			print json.dumps([ob.__dict__ for ob in signalSamples])
+			return json.dumps([ob.__dict__ for ob in signalSamples])
 			#print "Fingerprints: %d, unique signal samples: %d" % (fingerprintCount, len(uniqueSamples))
 			#self.printTimeDiff(start)
 
 	def printTimeDiff(self, start):
 		difference = Utils().getMillis() - start
 		print "Scanning took %dms" % difference 
-
-'''
-scanning witch channel by frequency: sudo iw dev wlan0 scan freq 2412
-
-                        * 2412 MHz [1] (20.0 dBm)
-                        * 2417 MHz [2] (20.0 dBm)
-                        * 2422 MHz [3] (20.0 dBm)
-                        * 2427 MHz [4] (20.0 dBm)
-                        * 2432 MHz [5] (20.0 dBm)
-                        * 2437 MHz [6] (20.0 dBm)
-                        * 2442 MHz [7] (20.0 dBm)
-                        * 2447 MHz [8] (20.0 dBm)
-                        * 2452 MHz [9] (20.0 dBm)
-                        * 2457 MHz [10] (20.0 dBm)
-                        * 2462 MHz [11] (20.0 dBm)
-                        * 2467 MHz [12] (20.0 dBm)
-                        * 2472 MHz [13] (20.0 dBm)
-                        * 2484 MHz [14] (disabled)
-'''

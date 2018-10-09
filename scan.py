6af152cd8ddb694b19ca3e41806500b649011b61
darek <darek@cosmose.co>
2018-10-09 13:52:08 +0200

    Changed zlib to gzip

--- scan.py
+++ scan.py
@@ -1,8 +1,12 @@
 from scanner import Scanner
-import zlib
+import gzip
 import base64
+import StringIO
 
 scanner = Scanner()
 resultJson = scanner.runScan()
-zipped = zlib.compress(resultJson)
+out = StringIO.StringIO()
+with gzip.GzipFile(fileobj=out, mode="w") as f:
+  f.write(resultJson)
+zipped = out.getvalue()
 print base64.b64encode(zipped)
